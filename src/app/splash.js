19a55e3c5cf90de68e5615c2b3c3592279b3ddca
jeonha01 <kangjeonha@email.com>
2025-07-17 05:54:11 +0900

    File - svg파일 추가

---
+++ src/app/splash.js
@@ -0,0 +1,43 @@
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import SVGBadge from '../assets/svgs/SVGBadge';
+
+const splash = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.iconWrapper}>
+                <SVGBadge />
+            </View>
+            <TouchableOpacity style={styles.kakaoButton}>
+                <Text style={styles.kakaoText}>카카오톡으로 시작하기</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#A3D9C3',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    iconWrapper: {
+        marginBottom: 32,
+        alignItems: 'center',
+    },
+    kakaoButton: {
+        backgroundColor: '#D9D9D9',
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 24,
+    },
+    kakaoText: {
+        color: '#000000',
+        fontSize: 20,
+        fontWeight: '100',
+        fontFamily: 'Inter_100Thin',
+        lineHeight: 20,
+    },
+});
+
+export default splash;
