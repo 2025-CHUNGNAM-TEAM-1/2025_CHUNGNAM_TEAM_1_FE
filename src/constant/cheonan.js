a73510aa22dc697aeb1fd3ba82a9b80050c10333
jeonha01 <kangjeonha@gmail.com>
2025-07-29 06:52:12 +0900

    Refactor - 마지막 코드 리팩토링

--- src/constant/cheonan.js
+++
@@ -1,9 +0,0 @@
-const CHEONAN_CENTER_POINTS = [
-    { latitude: 36.815110, longitude: 127.113799 },  // 천안시청 근처
-    { latitude: 36.783636, longitude: 127.152066 },  // 동남구 남부권
-    { latitude: 36.841917, longitude: 127.098616 },  // 서북구 북부권
-    { latitude: 36.792231, longitude: 127.165559 },  // 독립기념관 방향
-    { latitude: 36.805060, longitude: 127.130500 },  // 천안 중앙권
-];
-
-export default CHEONAN_CENTER_POINTS;
